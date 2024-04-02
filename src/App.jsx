7468476555b30ffb09084d42596c191e6f483a5f
jluchett <jluchett@hotmail.com>
2024-04-01 23:46:49 -0500

    Actializado paquetes y agregado MUI

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Welcome to my Ecommerce</h1>
-      <p className="read-the-docs">
+      <p className="">
         Es el inicio de este Proyecto, a por todo.
       </p>
     </>

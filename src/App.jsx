cbef785bc60730d9d65ed0a03546f0e30fe73e83
jluchett <jluchett@hotmail.com>
2024-04-02 23:46:59 -0500

    Creado estructura de carpetas y barra de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import Home from './pages/home/home'
 import './App.css'
 
 function App() {
   
   return (
     <>
-      <h1>Welcome to my Ecommerce</h1>
-      <p className="">
-        Es el inicio de este Proyecto, a por todo.
-      </p>
+      <Home/>
     </>
   )
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/navBar/navBar";
+
+const Home = () => {
+  return (
+    <body>
+      <NavBar/>
+      <h1>Welcome to my Ecommerce</h1>
+      <p className="">Es el inicio de este Proyecto, a por todo.</p>
+    </body>
+  );
+};
+
+export default Home;

76bd40906ae697cff32fe8cfece167f10a698a47
jluchett <jluchett@hotmail.com>
2024-04-06 23:40:12 -0500

    Limpiando componentes

---
+++ src/components/card/card.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const CardStandar
+ = () => {
+  return (
+    <div>
+      CardStandar
+
+    </div>
+  )
+}
+
+export default CardStandar
---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const navBar = () => {
+  return (
+    <div>navBar</div>
+  )
+}
+
+export default navBar
